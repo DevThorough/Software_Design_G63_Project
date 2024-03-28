89b84eba023d799121bba5f6b1e385d183de1d63
Devin <cblockdev@gmail.com>
2024-03-28 15:13:50 -0500

    fixed login session cookies errors

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 from .models import User, Profile
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db   ##means from __init__.py import db
@@ -19,6 +19,8 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
+                session['userID'] = user.id
+                session['loggedIn'] = True
                 return redirect(url_for('.profile', userID = user.id))
             else:
                 flash('Incorrect password, try again.', category='error')
@@ -28,7 +30,10 @@ def login():
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    session.pop('loggedIn', None)
+    session.pop('userID', None)
+    return redirect(url_for('.login'))
 
 @auth.route('/register', methods=['GET', 'POST'])
 def sign_up():
@@ -57,7 +62,8 @@ def sign_up():
 @auth.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
-    userID = request.args['userID']
+    if session['userID']:
+        userID = session['userID']
     if request.method == 'POST':
         fullName = request.form.get('fullname')
         address1 = request.form.get('address1')
@@ -77,7 +83,7 @@ def profile():
             profile.zipCode=zipCode
             db.session.commit()
             flash("Profile edited successfully!", category="success")
-            return redirect(url_for('.profile', userID = userID))
+            return redirect('/profile')
         else:
             #add profile to our database
             new_profile = Profile(fullName=fullName,address1=address1,address2=address2,
@@ -85,14 +91,14 @@ def profile():
             db.session.add(new_profile)
             db.session.commit()
             flash("Profile created successfully!", category="success")
-            return redirect(url_for('.profile', userID = userID))
+            return redirect('/profile')
     elif request.method == 'GET':
         profile = Profile.query.filter_by(user_id=userID).first()
         ##display profile if exist
         if profile:
-            return render_template("profile.html",userID=userID,onRecord = True,
+            return render_template("profile.html", onRecord = True,
                                    fullName = profile.fullName,address1 = profile.address1,
                                    address2 = profile.address2,city = profile.city,
                                    state = profile.state,zipCode = profile.zipCode)
         else:
-            return render_template("profile.html", userID = userID, onRecord=False)
+            return render_template("profile.html", onRecord=False)
