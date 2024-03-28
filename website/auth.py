16d9435bd498035b7e3a8744a0ac89008dd44440
Devin <cblockdev@gmail.com>
2024-03-28 02:09:21 -0500

    Jinjy Integration and Database V0.1

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,27 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db   ##means from __init__.py import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('User does not exist.', category='error')
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -16,13 +34,19 @@ def sign_up():
         username = request.form.get('username')
         password = request.form.get('password')
         
-        if len(username) < 2:
-            #flash("Username must contain at least 2 characters.", category='error')
-            pass
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash("Username already exist.", category='error')
+        elif len(username) < 2:
+            flash("Username must contain at least 2 characters.", category='error')
         elif len(password) < 3:
-            #flash("Password must contain at least 3 characters.", category='error')
-            pass
+            flash("Password must contain at least 3 characters.", category='error')
         else:
             #add user to our database
+            new_user = User(username=username, password=generate_password_hash(password, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
             flash("Account created successfully!", category="success")
-    return render_template("registration.html")
+            return redirect(url_for('views.home'))
+    return render_template("registration.html", user=current_user)
