0233864800d310cc16f84856484f83f979779308
Devin <cblockdev@gmail.com>
2024-03-30 13:54:57 -0500

    fuel_quote backend fix

--- website/fuel_quote.py
+++ website/fuel_quote.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, flash, redirect, session
-from .models import User, Profile
+from .models import User, Profile, FuelQuote
 from . import db   ##means from __init__.py import db
 from flask_login import login_required
 from .pricing import fuelPrice
@@ -14,7 +14,7 @@ def fqFunction():
     userID = session['userID']
     profile = Profile.query.filter_by(user_id=userID).first()
     if not profile:
-        flash('Create a profile to get a quote.', category='error')##Disable submit button if no profile
+        flash('Don't forget to create a profile!', category='error')##Disable submit button if no profile
 
     if request.method == 'POST':
         delivery_address = request.form.get('delivery_address')
@@ -22,7 +22,12 @@ def fqFunction():
         gallons = float(request.form.get('gallons_requested'))
         price = float(request.form.get('suggested_price'))
         total_amount_due = round(gallons * price,2)
-        ## save to db code here
+        ## add quote to quote history
+        new_quote = FuelQuote(delivery_address=delivery_address, delivery_date=delivery_date,
+                                gallons=gallons, price=price, total_amount_due=total_amount_due)
+        db.session.add(new_quote)
+        db.session.commit()
+        flash("Fuel Quote added to History.", category="success")
         return render_template("fuel_quote.html",gallons_requested=gallons, delivery_address=delivery_address,
                                 suggested_price = price, delivery_date=delivery_date, total_amount_due=total_amount_due,
                                 active="fuelQuoteNavLink")
