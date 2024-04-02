138cdbbfb25797342f72f5f7b5f51ea606ecc92f
CheickTT <tourecheick291@gmail.com>
2024-04-01 23:38:46 -0500

    error fixed

--- website/views.py
+++ website/views.py
@@ -1,13 +1,12 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 from .models import User, Profile
-from . import db   ##means from __init__.py import db
+from . import db   # means from __init__.py import db
 from flask_login import login_required, current_user
-
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    if(not session['loggedIn']):
-        session['loggedIn'] = False;
+    if 'loggedIn' not in session:
+        session['loggedIn'] = False
     return render_template("login.html", loggedIn=session['loggedIn'])
