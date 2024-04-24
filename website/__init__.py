5f040fcdfae5862ff668523c5e4a807efdc0ddd7
Merdi Mukenge <merdimukengend@gmail.com>
2024-04-24 16:44:17 -0500

    mongo database_m

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from pymongo import MongoClient
 from flask_login import LoginManager
+from .views import views
+from .auth import auth
+from .profile import profile
+from .fuel_quote import fuelquote
+from .fuel_quote_history import fq_history
+from .pricing import pricing
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -11,14 +18,12 @@ def create_app():
     app.config['SECRET_KEY'] = 'group63'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-    
-    from .views import views
-    from .auth import auth
-    from .profile import profile
-    from .fuel_quote import fuelquote
-    from .fuel_quote_history import fq_history
-    from .pricing import pricing
-    
+
+    # Connect to MongoDB
+    client = MongoClient('mongodb://localhost:27017/')
+    db_m = client['se_G63']
+
+
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(profile, url_prefix='/')
