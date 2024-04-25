4aeef3d057d97960358f9135a70aee9b35f323e3
Merdi Mukenge <merdimukengend@gmail.com>
2024-04-24 22:36:40 -0500

    model file fixed

--- website/__init__.py
+++ website/__init__.py
@@ -1,17 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-from pymongo import MongoClient
 from flask_login import LoginManager
-from .views import views
-from .auth import auth
-from .profile import profile
-from .fuel_quote import fuelquote
-from .fuel_quote_history import fq_history
-from .pricing import pricing
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
@@ -19,10 +13,12 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
-    # Connect to MongoDB
-    client = MongoClient('mongodb://localhost:27017/')
-    db_m = client['se_G63']
-
+    from .views import views
+    from .auth import auth
+    from .profile import profile
+    from .fuel_quote import fuelquote
+    from .fuel_quote_history import fq_history
+    from .pricing import pricing
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
@@ -44,5 +40,5 @@ def create_app():
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
-    
+
     return app
